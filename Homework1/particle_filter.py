8e17be775eec857cb34fe0243ec5c9dac845cb1b
AmazingAndrewM <michaea3@tcnj.edu>
2026-02-23 15:00:56 -0500

    Created a function to calculate the measurement covariance, which will be helpful when I implement the Kalman Filter.

--- Homework1/particle_filter.py
+++ Homework1/particle_filter.py
@@ -1,18 +1,16 @@
 from kalman_filter import KalmanFilter
 import pandas as pd
-import numpy
-
-DATA_FILE_PATH = "./data/Accelerometer.csv"
 
 def get_approximate_dt():
+    DATA_FILE_PATH = "./data/Accelerometer.csv"
     df_timestamps = pd.read_csv(DATA_FILE_PATH, header=0, usecols=["time"], converters={"time": str})
     series_diff = df_timestamps["time"].astype("int64").diff(periods=1)  # Break this into 2 steps because otherwise the Pandas default inferencing engine messes everything
     return series_diff.mean()
 
-def get_approximate_noise():
+def get_approximate_measurement_covariance():
+    DATA_FILE_PATH = "./still-data/Accelerometer.csv"
     df_acceleration = pd.read_csv(DATA_FILE_PATH, header=0, usecols=["x", "y", "z"], converters={"x": float, "y": float, "z": float})
-    variance = df_acceleration.var()
-    return variance
+    return df_acceleration.cov()
 
-noise_variance = get_approximate_noise()
-print(noise_variance)
+covariance = get_approximate_measurement_covariance()
+print(covariance)
