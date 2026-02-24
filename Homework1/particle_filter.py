33cea357de87278dee8f83d0dfd3a9358f1dc1ab
AmazingAndrewM <michaea3@tcnj.edu>
2026-02-24 17:04:14 -0500

    Finally got frame 0 of the particle filter to generate samples based on a probability field.

--- Homework1/particle_filter.py
+++ Homework1/particle_filter.py
@@ -1,10 +1,14 @@
 import argparse
 import json
 from PIL import Image
+import numpy
+import itertools
+from matplotlib import pyplot as plt
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 def get_masked_image(path: str):
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
     with Image.open(path) as image:
         rgb_image = image.convert("RGB")
         colors_in_image = rgb_image.getcolors(maxcolors=2)
@@ -19,9 +23,70 @@ def get_pixels_per_meter(path: str):
     pixels_per_meter = float(data["pixels_per_meter"])
     return pixels_per_meter
 
+def get_neighbors(coordinate: tuple[int, int], max_size: int):
+    x, y = coordinate
+
+    neighbors = []
+    if x - 1 >= 0:
+        neighbors.append((x - 1, y))
+    if x + 1 < max_size:
+        neighbors.append((x + 1, y))
+    if y - 1 >= 0:
+        neighbors.append((x, y - 1))
+    if y + 1 < max_size:
+        neighbors.append((x, y + 1))
+    return neighbors
+
 def generate_probability_gradient(mask_image: Image):
-    print(mask_image._check_size())
-    pass
+    # Make the center of the walkways more likely than the edges with BFS and do it in a relatively smooth way
+
+    distance_gradient = numpy.zeros(shape=mask_image.size, dtype=int)
+    has_visited = [[False for _ in range(mask_image.size[1])] for _ in range(mask_image.size[0])]
+
+    boundary_coordinates = [
+        coordinate
+        for coordinate in itertools.product(range(mask_image.size[0]), repeat=2)
+        if mask_image.getpixel(coordinate) == WHITE and any(
+            mask_image.getpixel(c) == BLACK 
+            for c in get_neighbors(coordinate, mask_image.size[0])
+        )
+    ]
+
+    for x, y in boundary_coordinates:
+        has_visited[x][y] = True
+
+    min_boundary_distance = 1
+    coordinates = boundary_coordinates
+    while len(coordinates) > 0:
+        new_coordinates = []
+        for coordinate in coordinates:
+            distance_gradient[coordinate[0], coordinate[1]] = min_boundary_distance
+
+            for n_coordinate in (
+                n for n in get_neighbors(coordinate, mask_image.size[0]) 
+                if mask_image.getpixel(n) == WHITE and not has_visited[n[0]][n[1]]
+            ):
+                new_coordinates.append(n_coordinate)
+                has_visited[n_coordinate[0]][n_coordinate[1]] = True
+                                 
+        min_boundary_distance += 1
+        coordinates = new_coordinates
+
+    return 1.0 / numpy.sum(distance_gradient) * distance_gradient
+
+def generate_samples(probabilities: numpy.ndarray, num_samples: int):
+    # Select samples based on the probabilities from with rng.choice()
+
+    rng = numpy.random.default_rng()
+    samples = rng.choice(
+        probabilities.shape[0] * probabilities.shape[1], 
+        num_samples,
+        True,
+        probabilities.ravel()
+    )
+
+    x, y = numpy.divmod(samples, probabilities.shape[0])
+    return numpy.column_stack((x, y))
 
 def main():
     parser = argparse.ArgumentParser(prog="Particle Filter")
@@ -33,9 +98,20 @@ def main():
     mask_image: Image = get_masked_image(mask_path)
 
     json_path = args.json
-    pixels_per_meter: float = get_pixels_per_meter(json_path)
+    # pixels_per_meter: float = get_pixels_per_meter(json_path)
 
-    generate_probability_gradient(mask_image)
+    gradient: numpy.ndarray = generate_probability_gradient(mask_image)
+
+    samples = generate_samples(gradient, 128)
+    plt.imshow(mask_image)
+    plt.scatter(samples[:, 0], samples[:, 1], marker="+", label="Samples")
+    plt.legend()
+    plt.show()
+
+    
+
+
+
 
 
 
