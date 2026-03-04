49a80fbf32fb84ebc38ca929f736ec968be32d1b
AmazingAndrewM1 <michaea3@tcnj.edu>
2026-03-04 00:25:31 -0500

    Created script for ease of grading with easy instructions in ReadME.md file.

--- runner.sh
+++ runner.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 cd ./Homework1
+echo "Running particle filter now!"
 python particle_filter.py --mask="./images/rutgers/WalkingMapMask.png" --json="./config.json"
-cd ..
+echo "Particle filter done. Check out Homework1/animation/animation.gif to see the output"
+deactivate
